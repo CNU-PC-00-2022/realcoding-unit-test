12cfc978223601a9db6a1c18c63c6c18d8b8cc40
Joe <outofchng@gmail.com>
2022-05-02 16:34:16 +0900

    Add calculator

--- src/__tests__/CalculatorDivide.spec.js
+++
@@ -1,8 +0,0 @@
-import Calculator from "../js/calculator/Calculator";
-
-describe("사직연산 나눗셈 테스트.", () => {
-  test("8 나누기 2는  4이다.", () => {
-    const calculator = new Calculator();
-    expect(calculator.divide(8, 2)).toEqual(4);
-  });
-});
