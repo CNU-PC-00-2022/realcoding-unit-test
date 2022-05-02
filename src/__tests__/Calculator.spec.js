508276cfc629908610337d5129ebfc76b772f5f9
sangho <USER@DESKTOP-LC9NV5E>
2022-05-02 16:53:12 +0900

    test sangho

--- src/__tests__/Calculator.spec.js
+++ src/__tests__/Calculator.spec.js
@@ -2,10 +2,10 @@ import Calculator from "../js/calculator/Calculator";
 
 const calc = new Calculator()
 
-describe("사직연산 덧셈 테스트.", () => {
+describe("사직연산 곱셈 테스트.", () => {
 
-  test("8과 2를 나누면 4이다.", () => {
-    expect(calc.divide(8, 2)).toEqual(4);
+  test("8과 2를 곱하면 16이다.", () => {
+    expect(calc.multiply(8, 2)).toEqual(16);
   });
 
 });
