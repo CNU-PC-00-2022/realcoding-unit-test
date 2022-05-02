0895061370cc93a165f2de718089fdc50321ef71
bae-sh <hyeon1293@gmail.com>
2022-05-02 18:04:39 +0900

    feat : 0 나누기 예외처리

--- src/__tests__/CalculatorDivide.spec.js
+++ src/__tests__/CalculatorDivide.spec.js
@@ -1,4 +1,4 @@
-import Calculator from "../js/calculator/Calculator";
+import Calculator, { DivideZeroError } from "../js/calculator/Calculator";
 
 describe("사직연산 나눗셈 테스트.", () => {
   test("9를 3으로 나누면 3이다.", () => {
@@ -7,6 +7,6 @@ describe("사직연산 나눗셈 테스트.", () => {
   });
   test("0으로 나누면 에러가 발생한다.", () => {
     const calculator = new Calculator();
-    expect(calculator.divide(9, 0)).toEqual(false);
+    expect(() => calculator.divide(9, 0)).toThrowError(DivideZeroError);
   });
 });
--- src/js/calculator/Calculator.js
+++ src/js/calculator/Calculator.js
@@ -1,3 +1,9 @@
+export class DivideZeroError extends Error {
+  constructor() {
+    super("0으로는 나눌 수 없지");
+    this.name = "Divide Zero Error";
+  }
+}
 class Calculator {
   plus = (a, b) => {
     return a + b;
@@ -8,14 +14,12 @@ class Calculator {
   multiply = (a, b) => {
     return a * b;
   };
+
   divide = (a, b) => {
-    if(b==0){
-      return false;
+    if (b == 0) {
+      throw new DivideZeroError();
     }
-    else{
-      return a / b;
-    }
-   
+    return a / b;
   };
 }
 
