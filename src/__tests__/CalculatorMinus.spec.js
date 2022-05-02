426adfacfda58fb22ab41bd4055c41007dbe38e5
rjsdn0124 <201802145@o.cnu.ac.kr>
2022-05-02 17:07:37 +0900

    d

--- src/__tests__/CalculatorMinus.spec.js
+++ src/__tests__/CalculatorMinus.spec.js
@@ -2,7 +2,7 @@ import Calculator from "../js/calculator/Calculator";
 
 describe("사직연산 덧셈 테스트.", () => {
   test("100에 9를 빼면 91이다.", () => {
-    const Calculator = new Calculator();
+    const calculator = new Calculator();
     expect(Calculator.minus(100, 9)).toEqual(91);
   });
 });
