67cb119a7a035a31e4eba8678bcb5ebfc9992346
juheon-jeong <jjh990427@gmail.com>
2022-05-02 16:34:30 +0900

    team-17 to feature/calculator

--- src/js/calculator/Calculator.js
+++ src/js/calculator/Calculator.js
@@ -2,6 +2,18 @@ class Calculator {
   plus = (a, b) => {
     return a + b;
   };
+  minus = (a, b) => {
+    return a - b;
+  }
+  multiply = (a, b) => {
+    return a * b;
+  }
+  divide = (a, b) => {
+    if (b == 0) {
+      return '0으로 값을 나눌 수 없습니다.'
+    }
+    return a / b;
+  } 
 }
 
 export default Calculator;
