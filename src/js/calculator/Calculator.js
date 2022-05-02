543e726de6602a40e60542723dba6bf7e9fadb4e
fist-99 <aveleedoo@gmail.com>
2022-05-02 17:30:39 +0900

    calculator

--- src/js/calculator/Calculator.js
+++ src/js/calculator/Calculator.js
@@ -2,6 +2,16 @@ class Calculator {
   plus = (a, b) => {
     return a + b;
   };
+  minus = (a,b) => {
+    return a - b;
+  }
+  multiply = (a,b) =>{
+    return a*b;
+  }
+  divide= (a,b) =>{
+    if(b===0) {return "error"}
+    return a / b;
+  }
 }
 
 export default Calculator;
