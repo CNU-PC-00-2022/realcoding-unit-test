07d3f55ffc285b16e2f24046d07b62ec2fe46928
zhzzang <leejh0799@gmail.com>
2022-05-02 17:50:13 +0900

    Calculator.js

--- src/js/calculator/Calculator.js
+++ src/js/calculator/Calculator.js
@@ -2,6 +2,23 @@ class Calculator {
   plus = (a, b) => {
     return a + b;
   };
+
+  minus = (a, b) => {
+    return a - b;
+  };
+
+  multiply = (a, b) => {
+    return a * b;
+  };
+
+  divide = (a,b) => {
+    if(b==0){
+      return false;
+    }else {
+      return a / b;
+    }
+  };
+  
 }
 
 export default Calculator;
