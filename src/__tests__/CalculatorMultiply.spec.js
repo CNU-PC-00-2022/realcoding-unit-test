6d5e4d3e883a792b415371ffe648d51700ed7f58
DESKTOP-FLPOG5V\강성엽 <piz115@naver.com>
2022-05-02 17:23:35 +0900

    real last commit

--- src/__tests__/CalculatorMultiply.spec.js
+++ src/__tests__/CalculatorMultiply.spec.js
@@ -1,8 +1,8 @@
 import Calculator from "../js/calculator/Calculator";
 
-describe("사직연산 곱셈 테스트.", () => {
+describe("사칙연산 곱셈 테스트.", () => {
   test("6과 3를 곱하면 18이다.", () => {
-    const Calculator = new Calculator();
-    expect(Calculator.multiply(6, 3)).toEqual(18);
+    const calculator = new Calculator();
+    expect(calculator.multiply(6, 3)).toEqual(18);
   });
 });
