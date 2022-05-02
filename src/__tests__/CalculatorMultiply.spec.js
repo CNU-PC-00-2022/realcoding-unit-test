ca1db0c320e379a7233784ffffba9c4ade5eb9b0
DESKTOP-FLPOG5V\강성엽 <piz115@naver.com>
2022-05-02 16:29:50 +0900

    multiply success

---
+++ src/__tests__/CalculatorMultiply.spec.js
@@ -0,0 +1,7 @@
+import Calculator from "../js/calculator/Calculator";
+
+describe("사직연산 덧셈 테스트.", () => {
+  test("6과 3를 곱하면 18이다.", () => {
+    expect(Calculator.multiply(6, 3)).toEqual(18);
+  });
+});
