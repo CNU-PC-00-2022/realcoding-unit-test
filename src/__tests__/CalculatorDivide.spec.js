091deb7478e14e00f3af03a90a19d1ab7f3b6a66
Joe <outofchng@gmail.com>
2022-05-02 17:59:23 +0900

    Add divide zero exception case

--- src/__tests__/CalculatorDivide.spec.js
+++ src/__tests__/CalculatorDivide.spec.js
@@ -1,8 +1,19 @@
-import Calculator from "../js/calculator/Calculator";
+import Calculator, { DivideZeroError } from "../js/calculator/Calculator";
 
 describe("사직연산 나눗셈 테스트.", () => {
   test("8 나누기 2를 하면 4이다.", () => {
     const calculator = new Calculator();
     expect(calculator.divide(8, 2)).toEqual(4);
   });
+});
+
+describe('사칙연산 나눗셈 테스트', () => { 
+  test('0으로 나누면 에러가 발생한다.', () => {
+    const calculator = new Calculator();
+    try {
+      calculator.divide(8, 0);
+    } catch(e) {
+      expect(e.name).toBe("Divide Zero Error");
+    }
+  }) 
 });
