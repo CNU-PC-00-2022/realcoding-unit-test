f3031cf6cabf1f46edc07949f1aff3287f3e8c81
rjsdn0124 <201802145@o.cnu.ac.kr>
2022-05-02 17:22:14 +0900

    asdf

--- src/__tests__/CalculatorMinus.spec.js
+++ src/__tests__/CalculatorMinus.spec.js
@@ -1,6 +1,6 @@
 import Calculator from "../js/calculator/Calculator";
 
-describe("사직연산 덧셈 테스트.", () => {
+describe("사칙연산 뺄셈 테스트.", () => {
   test("100에 9를 빼면 91이다.", () => {
     const calculator = new Calculator();
     expect(calculator.minus(100, 9)).toEqual(91);
