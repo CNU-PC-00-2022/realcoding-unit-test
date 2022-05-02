29f3822b0300427ced1d90188c74d1d92a3adbab
Kim Sanha <radium96@naver.com>
2022-05-02 17:03:52 +0900

    commit: divide

--- src/__tests__/Calculator.spec.js
+++ src/__tests__/Calculator.spec.js
@@ -4,8 +4,8 @@ const calc = new Calculator()
 
 describe("사직연산 덧셈 테스트.", () => {
 
-  test("8과 2를 나누면 4이다.", () => {
-    expect(calc.divide(8, 2)).toEqual(4);
+  test("10과 2를 나누면 5이다.", () => {
+    expect(calc.divide(10, 2)).toEqual(5);
   });
 
 });
--- src/__tests__/CalculatorDiv.spec.js
+++ src/__tests__/CalculatorDiv.spec.js
@@ -4,8 +4,8 @@ const calc = new Calculator()
 
 describe("사직연산 덧셈 테스트.", () => {
 
-  test("8과 2를 나누면 4이다.", () => {
-    expect(calc.divide(8, 2)).toEqual(4);
+  test("6과 2를 나누면 3이다.", () => {
+    expect(calc.divide(6, 2)).toEqual(3);
   });
 
 });
