a31da36815dd373c6f442e71e7ce63caacd849be
LeeSiHyeong <201802130@o.cnu.ac.kr>
2022-05-02 17:16:38 +0900

    team8 calc done

--- src/__tests__/Calculator.spec.js
+++ src/__tests__/Calculator.spec.js
@@ -2,13 +2,10 @@ import Calculator from "../js/calculator/Calculator";
 
 const calc = new Calculator()
 
-describe("사직연산 곱셈 테스트.", () => {
+describe("사직연산 덧셈 테스트.", () => {
 
-  test("10과 2를 나누면 5이다.", () => {
-    expect(calc.divide(10, 2)).toEqual(5);
-
-  test("8과 2를 곱하면 16이다.", () => {
-    expect(calc.multiply(8, 2)).toEqual(16);
+  test("10과 2를 더하면 12이다.", () => {
+    expect(calc.plus(10, 2)).toEqual(12);
   });
 
 });
