a71dfd7f826a95e6f1061ed6a88af2e1a42cb645
nub8p <sujin4418618@gmail.com>
2022-05-03 20:44:58 +0900

    "hw: team-9 202002488 "

--- src/js/calculator/Calculator.js
+++ src/js/calculator/Calculator.js
@@ -18,6 +18,18 @@ class Calculator {
     if(b===0) {throw new DivideZeroError();}
     return a / b;
   }
+  isNegative = (a) => {
+    return a < 0;
+  };
+  getDate = () => {
+    return new Date();
+  };
+  abs = (a) => {
+    return Math.abs(a);
+  };
+  customCalculation = (callback, ...args) => {
+    callback(...args);
+  };
 }
 
 export default Calculator;
