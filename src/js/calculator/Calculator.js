d5b8d44b837e0b676884d8f93c25903735315a50
fist-99 <aveleedoo@gmail.com>
2022-05-03 20:47:30 +0900

    hw2 202002523 add

--- src/js/calculator/Calculator.js
+++ src/js/calculator/Calculator.js
@@ -7,7 +7,7 @@ export class DivideZeroError extends Error {
 class Calculator {
   plus = (a, b) => {
     return a + b;
-  };
+  }
   minus = (a,b) => {
     return a - b;
   }
@@ -17,6 +17,18 @@ class Calculator {
   divide= (a,b) =>{
     if(b===0) {throw new DivideZeroError();}
     return a / b;
+  }
+  isNegative = (a) => {
+    return a < 0;
+  }
+  getDate = () => {
+    return new Date();
+  }
+  abs = (a) => {
+    return Math.abs(a);
+  }
+  customCalculation = (callback, ...args) => {
+    callback(...args);
   }
 }
 
