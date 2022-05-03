b7f5cbd84cda320b911765bb7d6ed669138cbda0
LeeSiHyeong <201802130@o.cnu.ac.kr>
2022-05-03 16:33:05 +0900

    homework

--- src/__tests__/CalculatorMultiply.spec.js
+++ src/__tests__/CalculatorMultiply.spec.js
@@ -2,6 +2,7 @@ import Calculator from "../js/calculator/Calculator";
 
 const calc = new Calculator()
 //test
+
 describe("사칙연산 곱셈 테스트.", () => {
 
   test("8과 2를 곱하면 16이다.", () => {
---
+++ src/__tests__/{201802130}.spec.js
@@ -0,0 +1,63 @@
+import Calculator from "../js/calculator/Calculator";
+
+const calc = new Calculator()
+
+/*
+    전체 테스트가 실행되기 전, 후의 동작 선언
+    각 테스트가 실행되기 전, 후의 동작 선언
+ */
+beforeAll(() => {
+    console.log("모든 테스트를 실행하기 전 한번만 실행된다.");
+});
+afterAll(() => {
+    console.log("모든 테스트가 완료된 후 한번만 실행된다.");
+});
+beforeEach(() => {
+    console.log("각 테스트를 실행하기 전 실행된다.")
+})
+afterEach(() => {
+    console.log("각 테스트가 완료된 후 실행된다.")
+})
+
+
+describe("음수 양수 판단", () => {
+    test("isNegative에 7을 넣으면 false를 반환한다.", () => {
+        expect(calc.isNegative(7)).toBeFalsy();
+    });
+
+    test("isNegative에 -3을 넣으면 true를 반환한다.", () => {
+        expect(calc.isNegative(-3)).toBeTruthy();
+    });
+    test("isNegative에 0을 넣으면 true가 아니다.", () => {
+        expect(calc.isNegative(0)).not.toBeTruthy();
+    });
+
+});
+
+describe("Date 클래스 판단하기", () => {
+    test("getDate를 하면 Date객체가 나온다.", () => {
+        expect(calc.getDate()).toBeInstanceOf(Date);
+    });
+});
+
+
+//추가과제
+describe("가상 함수 테스트해보기", () => {
+    test("customCalculation는 전달 된 함수를 호출시킨다.", () => {
+        const mockfn = jest.fn();
+        calc.customCalculation(mockfn);
+        expect(mockfn).toHaveBeenCalled();
+    });
+
+    test("customCalculation에 함수와 1, 2, 3인자를 전달하면, 전달 된 함수가 인자 1, 2, 3을 받아 호출한다.", () => {
+        const mockfn = jest.fn();
+        calc.customCalculation(mockfn, 1, 2, 3);
+        expect(mockfn).toHaveBeenCalledWith(1, 2, 3);
+    });
+
+    test("abs함수는 Math.abs를 호출한다.", () => {
+        const spyfn = jest.spyOn(Math, "abs");
+        calc.customCalculation(spyfn);
+        expect(spyfn).toHaveBeenCalled();
+    });
+})
