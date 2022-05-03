93e989c8b7981c4d814c6c24621ba4c8a9062aa0
juheon-jeong <jjh990427@gmail.com>
2022-05-03 20:21:15 +0900

    update assignment 201802155
    update assignment

--- src/__tests__/CalculatorDivide.spec.js
+++ src/__tests__/CalculatorDivide.spec.js
@@ -1,5 +1,18 @@
 import Calculator, { DivideZeroError } from "../js/calculator/Calculator";
+beforeAll(() => {
+  console.log('나눗셈 테스트 시작!')
+});
 
+afterAll(() => {
+  console.log('나눗셈 테스트 종료!')
+});
+beforeEach(() => {
+  console.log('나눗셈테스트 전 실행 됨');
+});
+
+afterEach(() => {
+  console.log('나눗셈테스트 후 실행 됨');
+})
 describe("사칙연산 나눗셈 테스트.", () => {
   test("6을 3으로 나누면 2이다.", () => {
     const calculator = new Calculator();
