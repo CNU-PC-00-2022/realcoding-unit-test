86357ba7631ee9145365f589140ec44fb9c8f686
HanWooSeok <hws1999@naver.com>
2022-05-04 17:00:29 +0900

    hw2 201802171

--- src/__tests__/201802171.spec.js
+++ src/__tests__/201802171.spec.js
@@ -1,5 +1,13 @@
 import Calculator, { DivideZeroError } from "../js/calculator/Calculator";
 
+beforeAll(() => {
+    console.log("모든 테스트를 실행하기 전 한번만 실행된다.");
+});
+  
+beforeEach(() => {
+    console.log("각 테스트를 실행하기 전 실행된다.");
+});
+
 describe("음수 양수 판단", () => {
   const calculator = new Calculator();
   test("isNegative에 7을 넣으면 false를 반환한다.", () => {
@@ -18,4 +26,12 @@ describe("Date 클래스 판단하기", () => {
   test("getDate를 하면 Date객체가 나온다.", () => {
     expect(calculator.getDate()).toBeInstanceOf(Date);
   });
+});
+
+afterEach(() => {
+    console.log("각 테스트가 완료된 후 실행된다.");
+});
+  
+afterAll(() => {
+    console.log("모든 테스트가 완료된 후 한번만 실행된다.");
 });
