14f457632a019d10e6ba5d45521c35bc08b48f53
SongJungHyun1004 <201802112@o.cnu.ac.kr>
2022-05-04 16:30:43 +0900

    hw2 201802112

--- src/__tests__/201802112.spec.js
+++ src/__tests__/201802112.spec.js
@@ -1,4 +1,10 @@
 import Calculator from "../js/calculator/Calculator";
+beforeAll(() => {
+    console.log("모든 테스트를 실행하기 전 한번만 실행된다.");
+});
+beforeEach(() => {
+    console.log("각 테스트를 실행하기 전 실행된다.");
+});
 
 describe("음수 양수 판단", () => {
     const cal = new Calculator();
@@ -18,4 +24,11 @@ describe("Date 클래스 판단하기", () => {
     test("getDate를 하면 Date객체가 나온다.", () => {
         expect(cal.getDate()).toBeInstanceOf(Date);
     });
+});
+
+afterEach(() => {
+    console.log("각 테스트가 완료된 후 실행된다.");
+});
+afterAll(() => {
+    console.log("모든 테스트가 완료된 후 한번만 실행된다.");
 });
