caba7898426e09ff1860ba5dc201fcdd7c3db595
SongJungHyun1004 <201802112@o.cnu.ac.kr>
2022-05-04 16:28:37 +0900

    hw1 201802112

--- src/__tests__/201802112.spec.js
+++ src/__tests__/201802112.spec.js
@@ -3,19 +3,19 @@ import Calculator from "../js/calculator/Calculator";
 describe("음수 양수 판단", () => {
     const cal = new Calculator();
     test("isNegative에 7을 넣으면 false를 반환한다.", () => {
-        expect(cal.isNegative(7));
+        expect(cal.isNegative(7)).toBeFalsy();
     });
     test("isNegative에 -3을 넣으면 true를 반환한다.", () => {
-        expect(cal.isNegative(-3));
+        expect(cal.isNegative(-3)).toBeTruthy();
     });
     test("isNegative에 0을 넣으면 true가 아니다.", () => {
-        expect(cal.isNegative(0)).toEqual(false);
+        expect(cal.isNegative(0)).not.toBeTruthy();
     });
 });
 
 describe("Date 클래스 판단하기", () => {
     const cal = new Calculator();
     test("getDate를 하면 Date객체가 나온다.", () => {
-        expect(cal.getDate());
+        expect(cal.getDate()).toBeInstanceOf(Date);
     });
 });
