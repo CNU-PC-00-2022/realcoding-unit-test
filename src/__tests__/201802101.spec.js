2677b7f9c3104e74a07a1f6e294e761d01618ce2
BaekMinHyeon <201802101@o.cnu.ac.kr>
2022-05-04 15:15:34 +0900

    hw1 201802101

---
+++ src/__tests__/201802101.spec.js
@@ -0,0 +1,21 @@
+import Calculator from "../js/calculator/Calculator";
+
+describe("음수 양수 판단", () => {
+    const cal = new Calculator();
+    test("isNegative에 10을 넣으면 false를 반환한다.", () => {
+        expect(cal.isNegative(10)).toBeFalsy();
+    });
+    test("isNegative에 -1을 넣으면 false를 반환한다.", () => {
+        expect(cal.isNegative(-1)).toBeTruthy();
+    });
+    test("isNegative에 0을 넣으면 true가 아니다.", () => {
+        expect(cal.isNegative(0)).not.toBeTruthy();
+    });
+});
+
+describe("Data 클래스 판단하기", () => {
+    test("getDate를 하면 Date 객체가 나온다.", () => {
+        const cal = new Calculator();
+        expect(cal.getDate()).toBeInstanceOf(Date);
+    })
+});
