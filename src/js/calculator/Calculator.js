2c66e47797117b7798f364795d0cc8c23b53ccb0
Joe <outofchng@gmail.com>
2022-05-05 22:54:33 +0900

    Resolve conflict

--- src/js/calculator/Calculator.js
+++ src/js/calculator/Calculator.js
@@ -9,36 +9,37 @@ class Calculator {
   plus = (a, b) => {
     return a + b;
   };
-<<<<<<< HEAD
 
   minus = (a, b) => {
     return a - b;
-  }
+  };
 
   multiply = (a, b) => {
     return a * b;
-  }
+  };
 
   divide = (a, b) => {
     if (b === 0) {
         throw new DivideZeroError();
     }
     return a / b;   
-  }
-=======
+  };
+
   isNegative = (a) => {
     return a < 0;
   };
+  
   getDate = () => {
     return new Date();
   };
+
   abs = (a) => {
     return Math.abs(a);
   };
+
   customCalculation = (callback, ...args) => {
     callback(...args);
   };
->>>>>>> eb79de69c369205e070bbbeafe241f179ccbef6e
 }
 
 export default Calculator;
