2fb5b952670d9a6d28dc374f3fe46ca64e8afdc2
Joe <outofchng@gmail.com>
2022-05-05 22:56:04 +0900

    Fix jest range

--- src/__tests__/CalculatorMinus.spec.js
+++ src/__tests__/CalculatorMinus.spec.js
@@ -1,8 +1,9 @@
 import Calculator from "../js/calculator/Calculator";
 
 describe("사직연산 뺄셈 테스트.", () => {
+  const calculator = new Calculator();
   test("3과 2를 빼면 1이다.", () => {
-    expect(3 - 2).toEqual(1);
+    expect(calculator.minus(2, 1)).toEqual(1);
   });
 
 });
