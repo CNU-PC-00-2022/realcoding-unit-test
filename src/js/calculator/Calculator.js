ad9cf83894845d58d7572c01d712607feba93fa8
Yujin_nKim <yjgugu5792@gmail.com>
2022-05-05 16:50:01 +0900

    fix cal

--- src/js/calculator/Calculator.js
+++ src/js/calculator/Calculator.js
@@ -1,6 +1,25 @@
+export class DivideZeroError extends Error {
+  constructor() {
+    super('0으로 나누면 에러가 발생한다.');
+    this.name = 'Divide Zero Error';
+  }
+}
+
 class Calculator {
   plus = (a, b) => {
     return a + b;
+  };
+  minus = (a, b) => {
+    return a - b;
+  };
+  multiply = (a, b) => {
+    return a * b;
+  };
+  divide = (a, b) => {
+    if (b == 0) {
+      throw new DivideZeroError();
+    }
+    return a / b;
   };
 }
 
