3cf9ae2eb69f7723c45eaa00ad4d63b0792c6736
nayoong02 <sonyangel02@naver.com>
2022-05-05 16:45:01 +0900

    과제 완료

--- src/__tests__/CalculatorMinus.spec.js
+++
@@ -1,8 +0,0 @@
-import Calculator from "../js/calculator/Calculator";
-
-describe("사직연산 뺄셈 테스트.", () => {
-  test("7에서 5를 빼면 2이다.", () => {
-    const calculator = new Calculator();
-    expect(calculator.minus(7, 5)).toEqual(2);
-  });
-});
