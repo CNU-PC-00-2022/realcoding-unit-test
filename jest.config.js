3a80acba838245aa359c00728ed87bca3bc302ef
zhzzang <leejh0799@gmail.com>
2022-05-05 16:44:00 +0900

    수정

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   testMatch: [
     "<rootDir>/**/__tests__/*.(js|jsx|ts|tsx)",
-  ],
+    "<rootDir>/**/__tests__/**/*.(js|jsx|ts|tsx)",
+],
   transformIgnorePatterns: ["<rootDir>/node_modules/"],
 };
