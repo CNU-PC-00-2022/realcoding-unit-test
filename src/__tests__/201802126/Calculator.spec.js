8383d7d8b3100fc222ca2cf1142916e15a5b9372
Joe <outofchng@gmail.com>
2022-05-08 21:09:53 +0900

    Refactor test code

--- src/__tests__/201802126/Calculator.spec.js
+++ src/__tests__/201802126/Calculator.spec.js
@@ -7,18 +7,18 @@ afterEach(() => console.log("각 테스트가 완료된 후 실행된다."));
 
 describe("음수 양수 판단", () => {
   test("isNegative에 7을 넣으면 false를 반환한다.", () => {
-    expect(new Calculator().isNegative(7)).toEqual(false);
+    expect(new Calculator().isNegative(7)).toBeFalsy();
   });
   test("isNegative에 -3을 넣으면 false를 반환한다.", () => {
-    expect(new Calculator().isNegative(-3)).toEqual(true);
+    expect(new Calculator().isNegative(-3)).toBeTruthy();
   });
   test("isNegative에 0을 넣으면 true가 아니다.", () => {
-    expect(new Calculator().isNegative(0)).toEqual(!true);
+    expect(new Calculator().isNegative(0)).not.toBeTruthy();
   });
 });
 
 describe("Date 클래스 판단하기", () => {
   test("getDate를 하면 Date객체가 나온다.", () => {
-    expect(new Calculator().getDate().constructor).toEqual(Date);
+    expect(new Calculator().getDate()).toBeInstanceOf(Date);
   });
 });
