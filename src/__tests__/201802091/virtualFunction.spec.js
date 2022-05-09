bf153b009b8b316231933114ea8e33546bf97a38
parkgwangwook <201802091@o.cnu.ac.kr>
2022-05-09 20:05:30 +0900

    virtual Function
    virtual Function

--- src/__tests__/201802091/virtualFunction.spec.js
+++ src/__tests__/201802091/virtualFunction.spec.js
@@ -1,20 +1,35 @@
-describe("가상 함수 테스트해보기", () => {
-    test("customCalculation는 전달 된 함수를 호출시킨다.", () => {
+import Calculator from "../../js/calculator/Calculator";
+beforeAll(() => {
+    console.log('가상함수 테스트 시작!')
+});
+
+afterAll(() => {
+    console.log('가상함수 테스트 종료!')
+});
+beforeEach(() => {
+    console.log('가상함수 테스트 전 실행 됨');
+});
+
+afterEach(() => {
+    console.log('가상함수 테스트 후 실행 됨');
+})
+describe("가상 함수 테스트해보기.", () => {
+    test("customCalculation는 전달 된 함수를 호출한다.", () => {
         const calculator = new Calculator();
-        const mockFunction = jest.fn();
-        calculator.customCalculation(mockFunction);
-        expect(mockFunction).toHaveBeenCalled();
+        const mockCallback = jest.fn();
+        calculator.customCalculation(mockCallback, ["1", "2"]);
+        expect(mockCallback).toHaveBeenCalled();
     });
     test("customCalculation에 함수와 1, 2, 3인자를 전달하면, 전달 된 함수가 인자 1, 2, 3을 받아 호출한다.", () => {
         const calculator = new Calculator();
-        const mockFunction = jest.fn();
-        calculator.customCalculation(mockFunction, 1, 2, 3);
-        expect(mockFunction).toHaveBeenCalledWith(1, 2, 3);
+        const mockCallback = jest.fn();
+        calculator.customCalculation(mockCallback, [1, 2, 3]);
+        expect(mockCallback).toHaveBeenCalledWith([1, 2, 3]);
     });
     test("abs함수는 Math.abs를 호출한다.", () => {
         const calculator = new Calculator();
-        const mockMathAbs = jest.spyOn(Math, "abs");
+        const spy = jest.spyOn(Math, "abs");
         calculator.abs(3);
-        expect(mockMathAbs).toHaveBeenCalled();
+        expect(spy).toBeCalledTimes(1);
     });
 });
