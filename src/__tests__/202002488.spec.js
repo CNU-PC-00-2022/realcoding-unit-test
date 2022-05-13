1e4d2e0ae043823d320496a28f648dd955ed7def
nub8p <sujin4418618@gmail.com>
2022-05-13 17:31:48 +0900

    10주차 과제 1

--- src/__tests__/202002488.spec.js
+++ src/__tests__/202002488.spec.js
@@ -37,3 +37,32 @@ describe("Date 클래스 판단하기", () => {
 afterAll(() => {
     console.log("모든 테스트가 완료된 후 한번만 실행된다.");
 });
+
+//추가 과제
+/*  jest.fn
+    jest.spyOn
+    expect.toHaveBeenCalled
+    expect.toHaveBeenCalledWith
+* */
+
+
+
+let calculator = new Calculator();
+
+describe("가상 함수 테스트해보기", () => {
+    test("customCalculation는 전달 된 함수를 호출시킨다.", () => {
+        const monkFn = jest.fn();
+        calculator.customCalculation(monkFn, 'a', 1);
+        expect(monkFn).toHaveBeenCalled();
+    });
+    test("customCalculation에 함수와 1, 2, 3인자를 전달하면, 전달 된 함수가 인자 1, 2, 3을 받아 호출한다.", () => {
+        const monkFn = jest.fn();
+        calculator.customCalculation(monkFn, 1, 2, 3);
+        expect(monkFn).toHaveBeenCalledWith(1, 2, 3);
+    });
+    test("abs함수는 Math.abs를 호출한다.", () => {
+        const spy = jest.spyOn(Math, "abs");
+        calculator.abs(-1);
+        expect(spy).toBeCalledTimes(1);
+    });
+});
